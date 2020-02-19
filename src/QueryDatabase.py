cda2d379f5ae03fc160b1eb91e1c878fbaffdcc3
wjsavage <willsavage2010@hotmail.com>
2020-02-19 11:45:02 +0000

    Added counting of rows, unique users, and unique items

--- src/QueryDatabase.py
+++ src/QueryDatabase.py
@@ -10,6 +10,21 @@ cur = connection.cursor()
 #     print(row)
 
 # print all lines where userID = 40714, and rating is less that 2.5
-criteria = (40714, 2.5,)
-for row in cur.execute('SELECT itemID FROM ratings WHERE userID = ? AND rating <= ?', criteria):
+# criteria = (40714, 2.5,)
+# for row in cur.execute('SELECT itemID FROM ratings WHERE userID = ? AND rating <= ?', criteria):
+#     print(row[0])
+
+# Count number of rows in database
+# cur.execute('SELECT * FROM ratings')
+# print(len(cur.fetchall()))
+# >>> 9425746
+
+# Count number of unique users
+# for row in cur.execute('SELECT COUNT(DISTINCT userID) FROM ratings;'):
+#     print(row[0])
+# >>> 274246
+
+# Count number of unique items
+for row in cur.execute('SELECT COUNT(DISTINCT itemID) FROM ratings;'):
     print(row[0])
+# >>> 19807
