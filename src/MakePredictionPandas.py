8f4b58b05fd290a6082778fe717c42213170c5d7
wjsavage <willsavage2010@hotmail.com>
2020-03-17 15:56:59 +0000

    Using pandas dataframes for holding data in memory, still need to re-write predictions function

--- src/MakePredictionPandas.py
+++ src/MakePredictionPandas.py
@@ -1,38 +1,35 @@
 import numpy as np
 import math
 import time
+import pandas as pd
 
 
 # uses the Pearson coefficient to calculate the similarity between 2 users
 # user_ratings_dict = {item_id, rating}
-def sim(user_ratings_dict, user_subset, cursor):
-    # given userId get a users average rating (rounded to 2dp)
-    u1_avg = sum(user_ratings_dict.values()) / len(user_ratings_dict)
+def calc_sim_scores(df, u1):
+    u1_avg = df.loc[u1]['rating'].mean()
+    u1_items = [y for x, y in df.index if x == u1]
+
+    user_subset = [x for x, y in df.index]
+    user_subset.remove(u1)
 
     sim_scores = []
 
-    count = 0
-
     for u2 in user_subset:
-        u2_ratings_dict = {}
+        # get shared items
+        u2_items = [y for x,y in df.index if x == u2]
+        shared_items = [s for s in u1_items if s in u2_items]
 
-        if count < 5:
-            db_start = time.time()
-        for row in cursor.execute(f'SELECT itemID, rating FROM {table_name} WHERE userID = ?', (u2,)):
-            u2_ratings_dict.update({row[0]: row[1]})
-        if count < 5:
-            db_end = time.time()
-        shared_items = [v for v in user_ratings_dict if v in u2_ratings_dict]
         # given a userId and the sharedItems return the list of ratings
 
-        u2_avg = sum(u2_ratings_dict.values()) / len(u2_ratings_dict)
+        u2_avg = df.loc[u2]['rating'].mean()
         # accumulator for 3 parts of sim equation
         a, b, c = 0, 0, 0
 
 
         for item in shared_items:
-            rating_u1 = user_ratings_dict[item] - u1_avg
-            rating_u2 = u2_ratings_dict[item] - u2_avg
+            rating_u1 = df.loc[(u1, item)]['rating'] - u1_avg
+            rating_u2 = df.loc[(u2, item)]['rating'] - u2_avg
 
             rating_u1_sq = rating_u1 * rating_u1
             rating_u2_sq = rating_u2 * rating_u2
@@ -84,28 +81,28 @@ def pred(user_ratings_dict, item_id, neighbours, cursor):
 
 # cur object is cursor for databases
 def get_prediction(user_id, item_id, table_nm, cursor):
-    global table_name
-    table_name = table_nm
-    # userRatings[0] = item, userRatings[1] = score
-    ratings_dict = {}
-    # given a userId get a list of (itemId, ratings) they've made
-    criteria = (user_id,)
-    for row in cursor.execute(f'SELECT itemID, rating FROM {table_name} WHERE userID = ?', criteria):
-        ratings_dict.update({row[0]: row[1]})
 
+    # given a userId get a list of (itemId, ratings) they've made
+    df = pd.DataFrame(columns=['userID', 'itemID', 'rating', 'time']).set_index(['userID', 'itemID'])
+    user_dict = {"userID": [], "itemID": [], "rating": [], "time": []}
+    for row in cursor.execute(f'SELECT itemID, rating, time FROM {table_nm} WHERE userID = ?', (user_id,)):
+        user_dict["userID"].append(user_id)
+        user_dict["itemID"].append(row[0])
+        user_dict["rating"].append(row[1])
+        user_dict["time"].append(row[2])
+    df = df.append(pd.DataFrame.from_dict(user_dict).set_index(['userID', 'itemID']))
 
     # check user hasn't already rated item
-    for item, rating in ratings_dict.items():
-        if item_id == item:
-            print("User already rated this item!")
-            return user_id, item_id, rating
-
+    if (user_id, item_id) in df.index:
+        print("User already rated this item!")
+        return df.loc[(user_id, item_id)]['rating']
     # database call - given userItems get a list of userId's who also have a rating for at least 1 of the items
     # userID and all the items they've rated, then look to see how of these are one of the items our user has rated
-    thresh = 30 if len(ratings_dict.items()) > 30 else len(ratings_dict.items())
+    items_rated = [y for x, y in df.index]
+    thresh = 30 if len(items_rated) > 30 else len(items_rated)
     # print("Thresh:", thresh)
     user_item_count = {}
-    for row in cursor.execute(f"SELECT userID FROM {table_name} WHERE itemID IN ({','.join(map(str, ratings_dict.keys()))}) "):
+    for row in cursor.execute(f"SELECT userID FROM {table_nm} WHERE itemID IN ({','.join(map(str, items_rated))})"):
         user_item_count[row[0]] = user_item_count.get(row[0], 0) + 1
 
     user_subset = []
@@ -113,40 +110,26 @@ def get_prediction(user_id, item_id, table_nm, cursor):
         if y >= thresh:
             user_subset.append(x)
     user_subset.remove(user_id)
-    before = len(user_item_count) - 1
-    after = len(user_subset)
-    # print("Users before:", before, ", Users after:", after, ", Difference:", before - after)
 
-    # print("User Subset", user_subset)
+    user_dict = {"userID": [], "itemID": [], "rating": [], "time": []}
+    for row in cursor.execute(f"SELECT userID, itemID, rating, time FROM {table_nm} WHERE userID IN ({','.join(map(str, user_subset))})"):
+        user_dict["userID"].append(row[0])
+        user_dict["itemID"].append(row[1])
+        user_dict["rating"].append(row[2])
+        user_dict["time"].append(row[3])
+    df = df.append(pd.DataFrame.from_dict(user_dict).set_index(['userID', 'itemID']))
 
-    # Initialise a list to store simScores
-    time_a = time.time()
-    sim_scores = sim(ratings_dict, user_subset, cursor)
-    time_b = time.time()
-    # print("Average sim time:", (time_b - time_a)/len(sim_scores))
-    # for compare_user_id in user_subset:
-    #     sim_score = sim(user_id, compare_user_id, cursor)
-    #     sim_scores.append((compare_user_id, sim_score))
-    
-    # print("Similarity scores between user and user subset:", sim_scores)
-
+    sim_scores = calc_sim_scores(df, user_id)
     # select the neighbourhood topN most similar users from the userSubset
     neighbours = []
     topN = 12_000 if len(sim_scores) > 12_000 else len(sim_scores)
-    # if < 2500 keep all, form 2000, 12000 scale, never more than 12,000
 
-    # print("topN:", topN)
-
-    # orders the sim_scores in accending order - then takes the sim_score indexes of the last N elements: N most similar users.
+    # orders the sim_scores in accending order - then takes the sim_score indexes of the last N elements:
+    # N most similar users.
     user_indexs = np.argsort([score[1] for score in sim_scores])[-topN:]
 
     for index in user_indexs:
         neighbours.append(sim_scores[index])
-    
-    # print("Users most similar neighbours:", neighbours)
 
-    time_a = time.time()
     result = pred(ratings_dict, item_id, neighbours, cursor)
-    time_b = time.time()
-    # print("Pred time per neighbour:", (time_b - time_a)/len(neighbours))
-    return result
+    return None # result
--- src/TestSpeed.py
+++ src/TestSpeed.py
@@ -3,7 +3,7 @@
 import sqlite3
 import os
 from time import time
-from src import MakePrediction
+from src import MakePrediction, MakePredictionPandas, MakePredictionV2
 
 name = "ratings"
 local_dir = os.path.dirname(__file__)
@@ -14,10 +14,20 @@ cur = connection.cursor()
 
 # Tests
 
+# start = time()
+# for i in range(10):
+#     _ = MakePrediction.get_prediction(1, 3, "ratings", cur)
+#     print(f"Made prediction: {i + 1}")
+# end = time()
+# avg_time = (end - start)/10
+# small_predictions = 496526
+# print(f"Time for, on average, per prediction = {round(avg_time, 4)} seconds")
+# print(f"Time for all small test predictions = {(avg_time * small_predictions) / 3600} hrs")
+
 start = time()
-for i in range(10):
-    _ = MakePrediction.get_prediction(1, 3, "ratings", cur)
-    print(f"Made prediction: {i + 1}")
+for i in range(1):
+    r = MakePredictionPandas.get_prediction(1, 3, "ratings", cur)
+    print(f"Made prediction: {i + 1}, Rating: {r}")
 end = time()
 avg_time = (end - start)/10
 small_predictions = 496526
@@ -27,6 +37,8 @@ print(f"Time for all small test predictions = {(avg_time * small_predictions) / 
 # Run 1, with no optimizations on 17/03 - still having about 4 db calls
 # Time for, on average, per prediction = 5.2845 seconds
 # Time for all small test predictions = 728.8528853302133 hrs
+
+# Run 2, going to store data of all users needed with a pandas dataframe
 
 # from src import MakePredictionV2
 # name = "ratingsV2"
