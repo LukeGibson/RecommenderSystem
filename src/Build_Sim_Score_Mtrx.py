5f06139eeedd8f550f1bba193b3b78957670586e
wjsavage <willsavage2010@hotmail.com>
2020-04-27 11:02:06 +0100

    Updating Build_Sim_Score_Mtrx.py

--- src/Build_Sim_Score_Mtrx.py
+++ src/Build_Sim_Score_Mtrx.py
@@ -6,6 +6,7 @@ import pandas as pd
 from tqdm import tqdm
 from math import sqrt
 from time import time
+import itertools
 
 
 # uses the pearson coefficient to calculate the similarity between 2 users
@@ -82,7 +83,8 @@ def get_top_users(user, df, item_dict):
 
 def main():
     db_name = "UserItemTables"
-    table_name = "User_table"
+    user_table_name = "User_table"
+    item_table_name = "Item_table"
     local_dir = os.path.dirname(__file__)
     db_path = os.path.join(local_dir, "..", "Data", "Databases", db_name + '.db')
     connection = sqlite3.connect(db_path)
@@ -90,10 +92,11 @@ def main():
 
     user_list = []
     item_dict = defaultdict(list)
-    for row in cursor.execute(f"SELECT userID, itemID FROM User_table"):
+    for row in cursor.execute(f"SELECT userID, itemID FROM {user_table_name}"):
         user_list.append(row[0])
-        item_dict.setdefault(row[1], []).append(row[0])
+        #item_dict.setdefault(row[1], []).append(row[0])
     user_list = list(dict.fromkeys(user_list))
+    print(len(user_list))
     print("> Built item_dict and user_list")
 
     scores_per_user = 30
@@ -105,7 +108,7 @@ def main():
 
     # print(f"Len of user list: {len(user_list)}")
 
-    for i in tqdm(range(len(user_list))): #tqdm(user_list):
+    for i in range(len(user_list)): #tqdm(user_list):
         user = user_list[i]
         s1 = time()
         # rem_users = user_list[user_list.index(user) + 1:]
@@ -113,47 +116,62 @@ def main():
         df = pd.DataFrame(columns=['userID', 'itemID', 'rating', 'time']).set_index(['userID', 'itemID'])
         user_dict = {"userID": [], "itemID": [], "rating": [], "time": []}
 
-        for row in cursor.execute(f'SELECT itemID, rating, time FROM {table_name} WHERE userID = {user}'):
+        for row in cursor.execute(f'SELECT itemID, rating, time FROM {user_table_name} WHERE userID = {user}'):
             user_dict["userID"].append(user)
             user_dict["itemID"].append(row[0])
             user_dict["rating"].append(row[1])
             user_dict["time"].append(row[2])
         df = df.append(pd.DataFrame.from_dict(user_dict).set_index(['userID', 'itemID']))
-        # print(f"1st DB call: {time() - s}")
+        print(f"1st DB call: {time() - s}")
 
         s = time()
-        user_subset = get_top_users(user, df, item_dict)
-        # print(f"Making user subset: {time() - s}")
+        items_rated = [y for x, y in df.index]
+        items_to_search = ','.join(map(str, items_rated))
+        top_users = []
+        for row in cursor.execute(
+                f'SELECT UserID, COUNT(UserID) AS User_Count FROM {item_table_name} ' +
+                f'WHERE ItemID IN ({items_to_search}) GROUP BY UserID ORDER BY User_Count DESC'):
+            # user_item_count[row[0]] = row[1]
+            if len(top_users) > 30:
+                break
+            u2 = row[0]
+            if not u2 == user:
+                top_users.append(row[0])
 
-        if len(user_subset) == 0:
-            print(f"Ah problem with user {user}")
-
-        # check if any of user subset < user
-        smaller_users = [u for u in user_subset if u < user]
-        # for these values check if sim score already calculated
-        for s in smaller_users:
-            idx = user_list.index(s)
-            if s in matrix_user[idx]:
-                print(matrix_user[idx])
-                print(matrix_scores[idx])
-                user_subset.remove(s)
-        # print(smaller_user)
-
-        # print(f"Total time: {time() - s1}")
-
+        print(f"2nd DB call: {time() - s}")
+        #
+        # if len(user_subset) == 0:
+        #     print(f"Ah problem with user {user}")
+        #
+        # # check if any of user subset < user
+        # smaller_users = [u for u in user_subset if u < user]
+        # # for these values check if sim score already calculated
+        # for s in smaller_users:
+        #     idx = user_list.index(s)
+        #     if s in matrix_user[idx]:
+        #         print(matrix_user[idx])
+        #         print(matrix_scores[idx])
+        #         user_subset.remove(s)
+        # # print(smaller_user)
+        #
+        # # print(f"Total time: {time() - s1}")
+        #
+        s = time()
         # append to df with the remaining users
         user_dict = {"userID": [], "itemID": [], "rating": [], "time": []}
         for row in cursor.execute(
-                f"SELECT userID, itemID, rating, time FROM {table_name} WHERE userID IN ({','.join(map(str, user_subset))})"):
+                f"SELECT userID, itemID, rating, time FROM {user_table_name} WHERE userID IN ({','.join(map(str, top_users))})"):
             user_dict["userID"].append(row[0])
             user_dict["itemID"].append(row[1])
             user_dict["rating"].append(row[2])
             user_dict["time"].append(row[3])
         df = df.append(pd.DataFrame.from_dict(user_dict).set_index(['userID', 'itemID']))
-
-        sim_scores = calc_sim_scores(df, user, user_subset, user_avg_ratings)
-        matrix_user[i, :] = [x for x, y in sim_scores]
-        matrix_scores[i, :] = [y for x, y in sim_scores]
+        print(f"3rd DB call: {time() - s}")
+        print()
+        #
+        # sim_scores = calc_sim_scores(df, user, user_subset, user_avg_ratings)
+        # matrix_user[i, :] = [x for x, y in sim_scores]
+        # matrix_scores[i, :] = [y for x, y in sim_scores]
 
         # add sim scores to matrix and users to second matrix at same positions
 
